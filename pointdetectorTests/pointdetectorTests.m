6d97e72ebdb9c191b30c31ee54f78cf789ff9aa0
spaghetti coder <appli@code.xii.jp>
2013-09-12 11:30:55 +0900

    Unit test system was changed GHUnit.

--- pointdetectorTests/pointdetectorTests.m
+++ pointdetectorTests/pointdetectorTests.m
@@ -1,47 +1,21 @@
 //
-//  pointdetectorTests.m
-//  pointdetectorTests
 //
-//  Created by  on 13/08/02.
-//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+// Template of Unit Test Case
+//
 //
 
-#import "pointdetectorTests.h"
+#import "/Users/sasaki/PointDetector-iOS/GHUnitIOS.framework/Versions/A/Headers/GHUnit.h"
+
+@interface pointdetectorTests : GHTestCase
+
+@end
 
 @implementation pointdetectorTests
 
-- (void)setUp
+- (void)testHogeFuga
 {
-    [super setUp];
     
-    // Set-up code here.
-    test_subject = [[TargetPoint alloc] init]; // not write retain because using ARC
-    defaults = [NSUserDefaults standardUserDefaults];
 }
 
-- (void)tearDown
-{
-    // Tear-down code here.
-    [super tearDown];
-}
-
-- (void)testNSUserDefaults
-{
-    
-    float targetLat = [defaults floatForKey:@"pointLatitude"];
-    float targetLng = [defaults floatForKey:@"pointLongitude"];
-    
-    if (!targetLat) {
-        STFail(@"目標地点の緯度が未保存");
-    }
-    if (!targetLng) {
-        STFail(@"目標地点の経度が未保存");
-    }
-    
-    if (targetLat == targetLng) {
-        STFail(@"緯度と経度が同じ値");
-    }
-    
-}
 
 @end
