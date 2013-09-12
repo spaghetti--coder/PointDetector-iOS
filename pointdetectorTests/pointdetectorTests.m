cdbada0bcbd700da581eac4a71a08d07ce80831e
spaghetti coder <appli@code.xii.jp>
2013-09-12 15:20:14 +0900

    test is updated

--- pointdetectorTests/pointdetectorTests.m
+++ pointdetectorTests/pointdetectorTests.m
@@ -4,7 +4,7 @@
 //
 //
 
-#import "/Users/sasaki/PointDetector-iOS/GHUnitIOS.framework/Versions/A/Headers/GHUnit.h"
+#import "GHUnitIOS/GHUnit.h"
 
 @interface pointdetectorTests : GHTestCase
 
@@ -12,10 +12,31 @@
 
 @implementation pointdetectorTests
 
-- (void)testHogeFuga
-{
-    
+// デフォルトはNOですが、UIのテストやメインスレッドに依存するテストを行う場合はYESにします。
+- (BOOL)shouldRunOnMainThread {
+    return NO;
 }
 
+// 本クラスが実行される前に呼び出されます。
+- (void)setUpClass {
+}
 
+// 本クラスが終了された後に呼び出されます。
+- (void)tearDownClass {
+}
+
+// 本クラスの各メソッドが実行される前に呼び出されます。
+- (void)setUp {
+}
+
+// 本クラスの各メソッドが終了された後に呼び出されます。
+- (void)tearDown {
+}
+
+// 「test～」というメソッド名にすることでテスト対象一覧に出力されます。
+- (void)testDivideRoundUp
+{
+    NSInteger num = 100;
+    GHAssertEquals(num, 34, @"test");
+}
 @end
