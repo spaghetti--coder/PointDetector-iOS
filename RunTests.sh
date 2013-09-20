2b33afd9c9f7e9d24f4346ba11dc9d33bd5808cd
spaghetti coder <appli@code.xii.jp>
2013-09-20 14:28:48 +0900

    fixed settings

--- RunTests.sh
+++ RunTests.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # If we aren't running from the command line, then exit
-if [ "$GHUNIT_CLI" = "" ] && [ "$GHUNIT_AUTORUN" = "" ]; then
-  exit 0
-fi
+#if [ "$GHUNIT_CLI" = "" ] && [ "$GHUNIT_AUTORUN" = "" ]; then
+#  exit 0
+#fi
 
 export DYLD_ROOT_PATH="$SDKROOT"
 export DYLD_FRAMEWORK_PATH="$CONFIGURATION_BUILD_DIR"
@@ -55,13 +55,14 @@ unset DYLD_ROOT_PATH
 unset DYLD_FRAMEWORK_PATH
 unset IPHONE_SIMULATOR_ROOT
 
-if [ -n "$WRITE_JUNIT_XML" ]; then
+#if [ -n "$WRITE_JUNIT_XML" ]; then
   MY_TMPDIR=`/usr/bin/getconf DARWIN_USER_TEMP_DIR`
   RESULTS_DIR="${MY_TMPDIR}test-results"
 
-  if [ -d "$RESULTS_DIR" ]; then
-	`$CP -r "$RESULTS_DIR" "$BUILD_DIR" && rm -r "$RESULTS_DIR"`
-  fi
-fi
+#  if [ -d "$RESULTS_DIR" ]; then
+#	`$CP -r "$RESULTS_DIR" "$BUILD_DIR" && rm -r "$RESULTS_DIR"`
+`$CP -r "$RESULTS_DIR" "$BUILD_DIR" && rm -r "$RESULTS_DIR"`
+#  fi
+#fi
 
 exit $RETVAL
